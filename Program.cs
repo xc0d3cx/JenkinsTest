6554ad5e93d3696bc9ce42400bc1c71031ed2236
Rob Bowman <xc0d3cx@users.noreply.github.com>
2021-02-19 15:42:44 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JenkinsTest
+{
+    class Program
+    {
+        static void Main(string[] args
+        {
+            Console.WriteLine("Jenkins Test 1");
+        }
+    }
+}

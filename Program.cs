8d582a87638e8bd370af71c93cb7529353ca8983
Robert Bowman <robert.bowman@ibm.com>
2021-02-19 16:53:14 -0800

    another test

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace JenkinsTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Jenkins Test 5");
+            Console.WriteLine("Jenkins Test 6");
         }
     }
 }
